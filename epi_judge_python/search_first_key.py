0f4ecffb2b0527dad65acc1426f15f5e42030d9d
Sebatian <ssarnecki34@gmail.com>
2025-07-06 16:32:42 +0200

    search first key

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -2,10 +2,19 @@ from typing import List
 
 from test_framework import generic_test
 
-
+# O(log n) time, O(1) space
 def search_first_of_k(A: List[int], k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    left, right, result = 0, len(A) - 1, -1
+    while left <= right:
+        mid = (left + right) // 2
+        if A[mid] > k:
+            right = mid - 1
+        elif A[mid] == k:
+            result = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+    return result
 
 
 if __name__ == '__main__':
