123b9a2d87fdccdc1eacc629ec92aa586bccef34
Sebatian <ssarnecki34@gmail.com>
2025-07-08 12:29:02 +0200

    intersect sorted arrays

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -1,12 +1,33 @@
+import re
 from typing import List
 
 from test_framework import generic_test
+import bisect
 
+# O(nm)
+def intersect_two_sorted_arrays_brute(A: List[int], B: List[int]) -> List[int]:
+    return [a for i,a in enumerate(A) if (i == 0 or a != A[i - 1]) and a in B]
+    
+# O(nlogm)
+def intersect_two_sorted_arrays_binary_search(A: List[int], B: List[int]) -> List[int]:
+    def is_present(k):
+       i = bisect.bisect_left(B, k) 
+       return i < len(B) and B[i] == k
+
+    return [a for i,a in enumerate(A) if (i == 0 or a != A[i -1]) and is_present(a)]
 
 def intersect_two_sorted_arrays(A: List[int], B: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
-
+    a_index, b_index, intersection_result = 0, 0, []
+    while a_index < len(A) and b_index < len(B):
+        if A[a_index] == B[b_index]:
+            if a_index == 0 or A[a_index] != A[a_index - 1]:
+                intersection_result.append(A[a_index])
+            a_index, b_index = a_index + 1, b_index + 1
+        elif A[a_index] < B[b_index]:
+            a_index+=1
+        else:
+            b_index+=1
+    return intersection_result
 
 if __name__ == '__main__':
     exit(
