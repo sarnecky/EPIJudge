eba99d3ed29773d213dca7ad4fe8476826d49afd
Sebatian <ssarnecki34@gmail.com>
2025-07-09 12:48:19 +0200

    sorted lists merge second approach

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -12,25 +12,24 @@ from test_framework import generic_test
 # current is property that keeps current node
 def merge_two_sorted_lists(L1: Optional[ListNode],
                            L2: Optional[ListNode]) -> Optional[ListNode]:
-    
-    dummy_head = ListNode();
-    current = dummy_head;
+    current_node = ListNode();
+    first_node = current_node
     while L1 and L2:
         if L1.data <= L2.data:
-            current.next = L1
-            current = L1;
+            current_node.next = L1
             L1 = L1.next
         else:
-            current.next = L2
-            current = L2
+            current_node.next = L2
             L2 = L2.next
-
-    current.next = L1 or L2
-    return dummy_head.next
-
+        current_node = current_node.next
+    
+    current_node.next = L1 or L2
+    return first_node.next
 
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main('sorted_lists_merge.py',
                                        'sorted_lists_merge.tsv',
                                        merge_two_sorted_lists))
+
+
