2bb7ed5061acddd153ad0db9c09136a5fb0bd951
Sebatian <ssarnecki34@gmail.com>
2025-07-09 14:20:37 +0200

    parity.py

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,9 +1,21 @@
 from test_framework import generic_test
 
 
+# O(n) n number of bits
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    parity = 0
+    while x:
+        parity ^= x & 1
+        x >>= 1
+    return parity % 2
+
+# O(k)
+def parity_bit_fiddler(x: int) -> int:
+    parity = 0
+    while x:
+        parity ^= 1
+        x &=(x - 1)
+    return parity % 2
 
 
 if __name__ == '__main__':
