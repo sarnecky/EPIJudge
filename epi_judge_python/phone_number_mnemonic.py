5342e1fd92f21531f5a1143f209bae09a9918375
Sebatian <ssarnecki34@gmail.com>
2025-07-10 14:11:35 +0200

    phone number mnemonics

--- epi_judge_python/phone_number_mnemonic.py
+++ epi_judge_python/phone_number_mnemonic.py
@@ -2,10 +2,23 @@ from typing import List
 
 from test_framework import generic_test, test_utils
 
+MAPPING = ('0', '1', 'ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ')
 
 def phone_mnemonic(phone_number: str) -> List[str]:
-    # TODO - you fill in here.
-    return []
+    def mnemonics_helper(digit: int) -> None:
+        if digit == len(phone_number):
+            mnemonics.append(''.join(partial_mnemonic))
+        else:
+            for c in MAPPING[int(phone_number[digit])]:
+                partial_mnemonic[digit] = c
+                mnemonics_helper(digit + 1)
+        return None
+
+    mnemonics: List[str] = []
+    partial_mnemonic = ['0'] * len(phone_number)
+    mnemonics_helper(0)
+    print(mnemonics)
+    return mnemonics
 
 
 if __name__ == '__main__':
