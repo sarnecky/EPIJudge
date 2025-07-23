b7e12844c4f2104eef1c86d585e4b4a567e3ae54
Sebatian <ssarnecki34@gmail.com>
2025-07-23 13:32:09 +0200

    number of score combinations

--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -5,8 +5,14 @@ from test_framework import generic_test
 
 def num_combinations_for_final_score(final_score: int,
                                      individual_play_scores: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    num_combinations_for_score = [[1] + [0] * final_score for _ in individual_play_scores]
+    for i in range(len(individual_play_scores)):
+        for j in range(1, final_score + 1):
+            score_for_previous_point = (num_combinations_for_score[i - 1][j] if i >=1  else 0)
+            score_only_for_current_point = (num_combinations_for_score[i][j - individual_play_scores[i]] 
+                if j >= individual_play_scores[i] else 0)
+            num_combinations_for_score[i][j] = score_for_previous_point + score_only_for_current_point
+    return num_combinations_for_score[-1][-1]
 
 
 if __name__ == '__main__':
