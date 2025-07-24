d867fac2117deb47351385d75927364417fddd3f
Sebatian <ssarnecki34@gmail.com>
2025-07-24 17:10:47 +0200

    search maze

--- epi_judge_python/search_maze.py
+++ epi_judge_python/search_maze.py
@@ -1,6 +1,7 @@
 import collections
 import copy
 import functools
+from pickle import TRUE
 from typing import List
 
 from test_framework import generic_test
@@ -14,8 +15,25 @@ Coordinate = collections.namedtuple('Coordinate', ('x', 'y'))
 
 def search_maze(maze: List[List[int]], s: Coordinate,
                 e: Coordinate) -> List[Coordinate]:
-    # TODO - you fill in here.
-    return []
+    def search_maze_helper(cur):
+        if not (0 <= cur.x < len(maze) and 0 <= cur.y < len(maze[cur.x]) and maze[cur.x][cur.y] == WHITE):
+            return False
+        path.append(cur)
+        maze[cur.x][cur.y] = BLACK
+
+        if cur == e:
+            return True
+        
+        for neightbour in map(Coordinate, (cur.x, cur.x, cur.x - 1, cur.x + 1), (cur.y - 1, cur.y + 1, cur.y, cur.y)):
+            if search_maze_helper(neightbour):
+                return True
+        del path [-1]
+        return False
+
+    path: List[Coordinate] = []
+    search_maze_helper(s)
+    print(path)
+    return path
 
 
 def path_element_is_feasible(maze, prev, cur):
