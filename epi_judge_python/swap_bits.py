09e82e9fd7494ff8bca8e628843acb3ba83c8397
Sebatian <ssarnecki34@gmail.com>
2025-08-01 12:03:34 +0200

    swap bits

--- epi_judge_python/swap_bits.py
+++ epi_judge_python/swap_bits.py
@@ -1,9 +1,17 @@
 from test_framework import generic_test
 
-
+# xorowanie z bit_mask zadziala dlatego ze:
+# w masce beda 2 bity w miejscach i oraz j
+# w liczbie x beda bity na miejscach i oraz j, a ich wartosci sa rozne
+# jezeli mamy maske i xorujemy ja z liczba x, efekt bedzie nastepujacy
+# tam gdzie w x bedzie 1, wynik xor 1 XOR 1 daje 0, czy wartosc przeciwna
+# tam gdzue w x bedzie 0, wynik xor 1 xor 0 daje 1, czyli wartosc przecina
+# bity sa zamienione
 def swap_bits(x, i, j):
-    # TODO - you fill in here.
-    return 0
+    if (x >> i) & 1 != ( x >> j) & 1:
+        bit_mask = (1 << i) | (1 << j) 
+        x ^= bit_mask
+    return x
 
 
 if __name__ == '__main__':
