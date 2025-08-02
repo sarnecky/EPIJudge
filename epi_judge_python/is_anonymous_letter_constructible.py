914ca076567157a6824c10fa11df26e3bffd9d8f
Sebatian <ssarnecki34@gmail.com>
2025-08-02 14:16:56 +0200

    is anonymous letter constructible

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -1,10 +1,19 @@
+import collections
 from test_framework import generic_test
 
 
 def is_letter_constructible_from_magazine(letter_text: str,
                                           magazine_text: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    letter_char_frequency = collections.Counter(letter_text)
+    for c in magazine_text:
+        if c in letter_char_frequency:
+            letter_char_frequency[c] -=1
+            if letter_char_frequency[c] == 0:
+                del letter_char_frequency[c]
+        if not letter_char_frequency:
+            return True
+        
+    return not letter_char_frequency
 
 
 if __name__ == '__main__':
