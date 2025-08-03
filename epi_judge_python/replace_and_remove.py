5f21bbc2d60c64733d7fec73cb5be0f0dda0efc7
Sebatian <ssarnecki34@gmail.com>
2025-08-03 15:06:20 +0200

    replace and remove

--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -1,4 +1,5 @@
 import functools
+from tkinter.constants import W
 from typing import List
 
 from test_framework import generic_test
@@ -6,8 +7,28 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def replace_and_remove(size: int, s: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+    a_count, write_idx = 0, 0
+    for i in range(size):
+        if s[i] == 'a':
+            a_count += 1
+        if s[i] != 'b':
+            s[write_idx] = s[i]
+            write_idx += 1
+
+    # backward iteration
+    current_index = write_idx - 1
+    write_idx = write_idx + a_count - 1
+    final_size = write_idx +1
+    while current_index >= 0:
+        if s[current_index] == 'a':
+            s[write_idx] = 'd'
+            s[write_idx - 1] = 'd'
+            write_idx -= 2
+        else:
+            s[write_idx] = s[current_index]
+            write_idx-=1
+        current_index-=1    
+    return final_size
 
 
 @enable_executor_hook
