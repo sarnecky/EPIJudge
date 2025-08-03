cd8c4228cc455364b141973f01a3f70eb4b63f6a
Sebatian <ssarnecki34@gmail.com>
2025-08-03 16:16:31 +0200

    search maze second

--- epi_judge_python/search_maze.py
+++ epi_judge_python/search_maze.py
@@ -15,24 +15,26 @@ Coordinate = collections.namedtuple('Coordinate', ('x', 'y'))
 
 def search_maze(maze: List[List[int]], s: Coordinate,
                 e: Coordinate) -> List[Coordinate]:
-    def search_maze_helper(cur):
+    
+    def seach_maze_helper(cur):
         if not (0 <= cur.x < len(maze) and 0 <= cur.y < len(maze[cur.x]) and maze[cur.x][cur.y] == WHITE):
             return False
+        
         path.append(cur)
         maze[cur.x][cur.y] = BLACK
 
         if cur == e:
             return True
         
-        for neightbour in map(Coordinate, (cur.x, cur.x, cur.x - 1, cur.x + 1), (cur.y - 1, cur.y + 1, cur.y, cur.y)):
-            if search_maze_helper(neightbour):
+        for c in map(Coordinate, (cur.x, cur.x, cur.x - 1, cur.x + 1), (cur.y + 1, cur.y - 1, cur.y, cur.y)):
+            if seach_maze_helper(c):
                 return True
-        del path [-1]
-        return False
 
+        del path[-1]
+        return False
+        
     path: List[Coordinate] = []
-    search_maze_helper(s)
-    print(path)
+    seach_maze_helper(s)
     return path
 
 
