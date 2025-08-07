346a5f950e619c453a90224c9ca49056d3e1cc17
Sebatian <ssarnecki34@gmail.com>
2025-08-07 13:28:29 +0200

    reverse sublist

--- epi_judge_python/reverse_sublist.py
+++ epi_judge_python/reverse_sublist.py
@@ -3,11 +3,23 @@ from typing import Optional
 from list_node import ListNode
 from test_framework import generic_test
 
-
+# important to keep sublist_head
 def reverse_sublist(L: ListNode, start: int,
                     finish: int) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    dummy_head = sublist_head = ListNode(0, L)
+    for _ in range(1, start):
+        sublist_head = sublist_head.next
+
+    # sublist_head -> pointer to head before sublist, sublist_head.next -> first element of the sublist
+    # working_pointer -> element which will be moved forward
+    # node to be extracted -> element which will be moved backward
+    working_pointer = sublist_head.next
+    for _ in range(finish - start):
+        node_to_be_extaxcted = working_pointer.next
+        working_pointer.next = node_to_be_extaxcted.next
+        node_to_be_extaxcted.next = sublist_head.next
+        sublist_head.next = node_to_be_extaxcted
+    return dummy_head.next
 
 
 if __name__ == '__main__':
