74754fe47bd3a808ac51d8ecff514b7516cc5026
Sebatian <ssarnecki34@gmail.com>
2025-08-09 14:17:35 +0200

    matrix connected regions

--- epi_judge_python/matrix_connected_regions.py
+++ epi_judge_python/matrix_connected_regions.py
@@ -1,10 +1,21 @@
+import collections
 from typing import List
 
 from test_framework import generic_test
 
 
 def flip_color(x: int, y: int, image: List[List[bool]]) -> None:
-    # TODO - you fill in here.
+    queue = collections.deque([(x, y)])
+    current_color = image[x][y]
+    while queue:
+        x, y = queue.popleft()
+        image[x][y] = not current_color
+
+        for next_x, next_y in ((x, y +1), (x, y - 1), (x +1, y), (x - 1, y)):
+            if (0 <= next_x < len(image) and
+                0 <= next_y < len(image[next_x]) and
+                image[next_x][next_y] == current_color):
+                    queue.append((next_x, next_y))
     return
 
 
