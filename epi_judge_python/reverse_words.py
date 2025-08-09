40e76a2fa5ac3798fc953983845a88689ab46d04
Sebatian <ssarnecki34@gmail.com>
2025-08-09 14:31:05 +0200

    reverse_words.py

--- epi_judge_python/reverse_words.py
+++ epi_judge_python/reverse_words.py
@@ -1,4 +1,5 @@
 import functools
+from tracemalloc import start
 
 from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
@@ -7,8 +8,26 @@ from test_framework.test_utils import enable_executor_hook
 # Assume s is a list of strings, each of which is of length 1, e.g.,
 # ['r', 'a', 'm', ' ', 'i', 's', ' ', 'c', 'o', 's', 't', 'l', 'y'].
 def reverse_words(s):
-    # TODO - you fill in here.
-    return
+    def reverse_range(s, start, finish):
+        while start < finish:
+            s[start], s[finish] = s[finish], s[start]
+            start, finish = start + 1, finish - 1
+
+    reverse_range(s, 0, len(s) - 1) # first of all, reverse entire stirng
+
+    start = 0
+    while True:
+        finish = start
+        while finish < len(s) and s[finish] != ' ':
+            finish+=1
+        
+        if finish == len(s):
+            break
+
+        reverse_range(s, start, finish -1)
+
+        start = finish+1
+    reverse_range(s, start, len(s) - 1) # reverse laset word
 
 
 @enable_executor_hook
