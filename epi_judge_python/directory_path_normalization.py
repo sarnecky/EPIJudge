7d02d4d6ded8b105756a555a958b625d60af08e5
Sebatian <ssarnecki34@gmail.com>
2025-08-12 21:40:24 +0200

    direcotry path normalization

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -1,9 +1,28 @@
+from ntpath import join
 from test_framework import generic_test
 
 
 def shortest_equivalent_path(path: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    if not path:
+        raise ValueError('Empty string is not valid path')
+
+    path_names = []
+    if path[0] == '/':
+        path_names.append('/')
+
+    for token in (token for token in path.split('/') if token not in ['.', '']):
+        if token == '..':
+            if not path_names or path_names[-1] == '..':
+                path_names.append(token)
+            else:
+                if path_names[-1] == '/':
+                    raise ValueError('error path')
+                path_names.pop()
+        else: # must be a name
+            path_names.append(token)
+
+    result = '/'.join(path_names)
+    return result[result.startswith('//'):]
 
 
 if __name__ == '__main__':
