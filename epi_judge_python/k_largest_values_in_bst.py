5b76ef42392cefe3e133470e9f75083b6eb753b1
Sebatian <ssarnecki34@gmail.com>
2025-08-17 14:03:47 +0200

    k largest values in bst

--- epi_judge_python/k_largest_values_in_bst.py
+++ epi_judge_python/k_largest_values_in_bst.py
@@ -5,8 +5,24 @@ from test_framework import generic_test, test_utils
 
 
 def find_k_largest_in_bst(tree: BstNode, k: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    def find_largest_helper(tree: BstNode): # revese in order traversal
+        if not tree:
+            return;
+        
+        find_largest_helper(tree.right)
+
+        if len(largest_elements) < k:
+            largest_elements.append(tree.data)
+        else:
+            return;
+
+        find_largest_helper(tree.left)
+
+        return;
+    
+    largest_elements: List[int] = []
+    find_largest_helper(tree)
+    return largest_elements;
 
 
 if __name__ == '__main__':
