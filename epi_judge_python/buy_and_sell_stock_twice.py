2368eaf571fe363612f98290d6a9cfd562e9c1a1
Sebatian <ssarnecki34@gmail.com>
2025-08-20 18:42:15 +0200

    buy and sell stock twice

--- epi_judge_python/buy_and_sell_stock_twice.py
+++ epi_judge_python/buy_and_sell_stock_twice.py
@@ -4,8 +4,18 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_twice(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    max_total_profit, min_price_so_far = 0.0, float('inf')
+    first_buy_profits = [0.0] * len(prices)
+    for i, price in enumerate(prices): # forward phase, each day has a profit if we sell on that day
+        min_price_so_far = min(min_price_so_far, price)
+        max_total_profit = max(max_total_profit, price - min_price_so_far)
+        first_buy_profits[i] = max_total_profit
+
+    max_profit_so_far = float('-inf')
+    for i, price in reversed(list(enumerate(prices[1:], 1))):
+        max_profit_so_far = max(max_profit_so_far, price)
+        max_total_profit = max(max_total_profit, first_buy_profits[i] + max_profit_so_far - price)
+    return max_total_profit
 
 
 if __name__ == '__main__':
