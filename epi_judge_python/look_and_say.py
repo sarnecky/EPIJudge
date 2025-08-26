e765465e7b45486eea7bca8464b7abfb2901ca57
Sebatian <ssarnecki34@gmail.com>
2025-08-26 21:20:46 +0200

    look and say problem

--- epi_judge_python/look_and_say.py
+++ epi_judge_python/look_and_say.py
@@ -1,9 +1,14 @@
+import itertools
 from test_framework import generic_test
 
 
 def look_and_say(n: int) -> str:
-    # TODO - you fill in here.
-    return ''
+    s = '1'
+    for _ in range(n -1):
+        s = ''.join(
+           str(len(list(group))) + key for key, group in itertools.groupby(s)
+        )
+    return s
 
 
 if __name__ == '__main__':
