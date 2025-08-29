5b15df8ac10beab514fcc60d09e26cd3a28386be
Sebatian <ssarnecki34@gmail.com>
2025-08-29 17:07:28 +0200

    k closest stars

--- epi_judge_python/k_closest_stars.py
+++ epi_judge_python/k_closest_stars.py
@@ -1,6 +1,7 @@
 import functools
+import heapq
 import math
-from typing import Iterator, List
+from typing import Iterator, List, Tuple
 
 from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
@@ -28,8 +29,12 @@ class Star:
 
 
 def find_closest_k_stars(stars: Iterator[Star], k: int) -> List[Star]:
-    # TODO - you fill in here.
-    return []
+    max_heap: List[Tuple[float, Star]] = []
+    for star in stars:
+        heapq.heappush(max_heap, (-star.distance, star))
+        if len(max_heap) == k + 1:
+            heapq.heappop(max_heap)
+    return [star for _, star in heapq.nlargest(k, max_heap)]
 
 
 def comp(expected_output, output):
