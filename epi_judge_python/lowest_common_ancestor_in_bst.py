0ae835bb27809aa3a393b3ee9c0aff62c280200d
Sebatian <ssarnecki34@gmail.com>
2025-09-06 19:32:56 +0200

    lowest common ancestor in bst

--- epi_judge_python/lowest_common_ancestor_in_bst.py
+++ epi_judge_python/lowest_common_ancestor_in_bst.py
@@ -10,8 +10,13 @@ from test_framework.test_utils import enable_executor_hook
 
 # Input nodes are nonempty and the key at s is less than or equal to that at b.
 def find_lca(tree: BstNode, s: BstNode, b: BstNode) -> Optional[BstNode]:
-    # TODO - you fill in here.
-    return None
+    while tree:
+        if tree.data > b.data and tree.data > s.data:
+            tree = tree.left
+        elif tree.data < s.data and tree.data < b.data:
+            tree = tree.right
+        else:
+            return tree
 
 
 @enable_executor_hook
