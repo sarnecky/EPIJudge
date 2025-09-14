bd0f4d069c5ac0e2fc598d631cae3aac0c37b575
Sebatian <ssarnecki34@gmail.com>
2025-09-14 11:40:41 +0200

    smallest subaray covering set

--- epi_judge_python/smallest_subarray_covering_set.py
+++ epi_judge_python/smallest_subarray_covering_set.py
@@ -11,8 +11,61 @@ Subarray = collections.namedtuple('Subarray', ('start', 'end'))
 
 def find_smallest_subarray_covering_set(paragraph: List[str],
                                         keywords: Set[str]) -> Subarray:
-    # TODO - you fill in here.
-    return Subarray(0, 0)
+    class DoublyLinkedListNode:
+        def __init__(self, data=None) -> None:
+            self.data = data
+            self.next = None
+            self.prev = None
+
+    class LinkedList:
+        def __init__(self) -> None:
+            self.head = None
+            self.tail = None
+            self._size = 0
+
+        def __len__(self):
+            return self._size
+
+        def insert_after(self, value):
+            node  = DoublyLinkedListNode(value)
+            node.prev = self.tail
+            if self.tail:
+                self.tail.next = node
+            else:
+                self.head = node
+            self.tail = node
+            self._size += 1
+
+        def remove(self, node):
+            if node.next:
+                node.next.prev = node.prev
+            else:
+                self.tail = node.prev
+            
+            if node.prev:
+                node.prev.next = node.next
+            else:
+                self.head = node.next
+            node.next = node.prev = None
+            self._size -=1
+    
+    loc = LinkedList()
+    d = { s : None for s in keywords }
+    result = Subarray(-1, -1)
+    for idx, s in enumerate(paragraph):
+        if s in d: # if s exists in keywords
+            it = d[s]
+            if it is not None:
+                loc.remove(it)
+
+            loc.insert_after(idx)
+            d[s] = loc.tail
+
+        if len(loc) == len(keywords):
+            if result == (-1, -1) or result[1] - result[0] > idx - loc.head.data:
+                result = Subarray(loc.head.data, idx)
+
+    return result
 
 
 @enable_executor_hook
