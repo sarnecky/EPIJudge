99a8108cb8b411343582a23afc53291380cf8691
Sebatian <ssarnecki34@gmail.com>
2025-09-21 12:43:25 +0200

    roman to integer

--- epi_judge_python/roman_to_integer.py
+++ epi_judge_python/roman_to_integer.py
@@ -1,9 +1,18 @@
+import functools
 from test_framework import generic_test
 
-
+# LIX
+# XIL
+0, 1, 2
 def roman_to_integer(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    T = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
+    result = T[s[-1]]
+    for i in reversed(range(len(s)-1)):
+        if T[s[i]] < T[s[i +1]]:
+            result-= T[s[i]]
+        else:
+            result+=T[s[i]]
+    return result
 
 
 if __name__ == '__main__':
