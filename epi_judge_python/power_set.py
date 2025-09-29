21423382400cf7258d7762f385194e627d3cfb98
Sebatian <ssarnecki34@gmail.com>
2025-09-29 14:27:44 +0200

    power set

--- epi_judge_python/power_set.py
+++ epi_judge_python/power_set.py
@@ -4,8 +4,17 @@ from test_framework import generic_test, test_utils
 
 
 def generate_power_set(input_set: List[int]) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    def directed_power_set(to_be_selected, selected_so_far):
+        if to_be_selected == len(input_set):
+            power_set.append(selected_so_far)
+            return
+        
+        directed_power_set(to_be_selected + 1, selected_so_far)
+        directed_power_set(to_be_selected + 1, selected_so_far + [input_set[to_be_selected]])
+    
+    power_set: List[List[int]] = []
+    directed_power_set(0, [])
+    return power_set
 
 
 if __name__ == '__main__':
