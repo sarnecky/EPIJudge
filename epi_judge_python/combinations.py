4c2858b072e3ad8d8e4767b9c782530fc13d39a6
Sebatian <ssarnecki34@gmail.com>
2025-09-30 18:53:34 +0200

    combinations

--- epi_judge_python/combinations.py
+++ epi_judge_python/combinations.py
@@ -4,8 +4,20 @@ from test_framework import generic_test, test_utils
 
 
 def combinations(n: int, k: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    def directed_combinations(offset, partial_combination):
+        if len(partial_combination) == k:
+            result.append(partial_combination.copy())
+            return
+        
+        num_remaining = k - len(partial_combination)
+        i = offset
+        while i <= n and num_remaining <= n - i + 1:
+            directed_combinations(i + 1, partial_combination + [i])
+            i+=1
+    
+    result: List[List[int]] = []
+    directed_combinations(1, []);
+    return result
 
 
 if __name__ == '__main__':
