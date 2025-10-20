9e4ab6d1b199b68d27fda2455439969cdf79a4f9
Sebatian <ssarnecki34@gmail.com>
2025-10-20 10:54:05 +0200

    apply permutation

--- epi_judge_python/apply_permutation.py
+++ epi_judge_python/apply_permutation.py
@@ -4,8 +4,10 @@ from test_framework import generic_test
 
 
 def apply_permutation(perm: List[int], A: List[int]) -> None:
-    # TODO - you fill in here.
-    return
+    for i in range(len(A)):
+        while perm[i] != i:
+            A[perm[i]], A[i] = A[i], A[perm[i]]
+            perm[perm[i]], perm[i] = perm[i], perm[perm[i]]
 
 
 def apply_permutation_wrapper(perm, A):
