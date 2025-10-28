efef8a3b97f8da73d4ea437b110372b0851e984b
Sebatian <ssarnecki34@gmail.com>
2025-10-28 11:26:17 +0100

    delete kth last from list

--- epi_judge_python/delete_kth_last_from_list.py
+++ epi_judge_python/delete_kth_last_from_list.py
@@ -6,8 +6,17 @@ from test_framework import generic_test
 
 # Assumes L has at least k nodes, deletes the k-th last node in L.
 def remove_kth_last(L: ListNode, k: int) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    dummy_head = ListNode(0, L)
+    first = dummy_head.next
+    second = dummy_head
+    for _ in range(k):
+        first = first.next
+
+    while first:
+        first = first.next
+        second = second.next
+    second.next = second.next.next
+    return dummy_head.next
 
 
 if __name__ == '__main__':
