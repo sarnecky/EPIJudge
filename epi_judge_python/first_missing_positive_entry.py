3c580a0a68628b0c78236facd3996070fc9f2c59
Sebatian <ssarnecki34@gmail.com>
2025-10-29 23:01:59 +0100

    first missing positive entry

--- epi_judge_python/first_missing_positive_entry.py
+++ epi_judge_python/first_missing_positive_entry.py
@@ -4,8 +4,17 @@ from test_framework import generic_test
 
 
 def find_first_missing_positive(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    i = 0
+    while i < len(A):
+        if 1 <= A[i] <= len(A) and A[i] != A[A[i] -1]:
+            A[A[i] - 1], A[i] = A[i], A[A[i] - 1]
+        else:
+            i += 1
+
+    for i, e in enumerate(A):
+        if e != i + 1:
+            return i + 1
+    return len(A) + 1
 
 
 if __name__ == '__main__':
