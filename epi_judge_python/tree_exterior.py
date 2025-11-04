b108fce446740a098771c726111b73ac38929986
Sebatian <ssarnecki34@gmail.com>
2025-11-04 11:01:16 +0100

    tree exterior

--- epi_judge_python/tree_exterior.py
+++ epi_judge_python/tree_exterior.py
@@ -8,8 +8,42 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def exterior_binary_tree(tree: BinaryTreeNode) -> List[BinaryTreeNode]:
-    # TODO - you fill in here.
-    return []
+    def left_boundary(subtree):
+        if not subtree or (not subtree.left and not subtree.right):
+            return
+        exterior.append(subtree)
+        if subtree.left:
+            left_boundary(subtree.left)
+        else:
+            left_boundary(subtree.right)
+
+    def right_boundary(subtree):
+        if not subtree or (not subtree.left and not subtree.right):
+            return
+        if subtree.right:
+            right_boundary(subtree.right)
+        else:
+            right_boundary(subtree.left)
+        exterior.append(subtree)
+
+    def leaves(subtree):
+        if not subtree:
+            return
+        if not subtree.left and not subtree.right:
+            exterior.append(subtree)
+            return
+        leaves(subtree.left)
+        leaves(subtree.right)
+
+    if not tree:
+        return []
+    exterior = [tree]
+    left_boundary(tree.left)
+    leaves(tree.left)
+    leaves(tree.right)
+    right_boundary(tree.right)
+
+    return exterior
 
 
 def create_output_list(L):
