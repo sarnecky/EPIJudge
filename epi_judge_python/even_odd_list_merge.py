260b78f0cdc7174f0b42224668543c90ac85e02a
Sebatian <ssarnecki34@gmail.com>
2025-11-12 22:34:55 +0100

    even odd list merge

--- epi_judge_python/even_odd_list_merge.py
+++ epi_judge_python/even_odd_list_merge.py
@@ -1,3 +1,4 @@
+from pickle import NONE
 from typing import Optional
 
 from list_node import ListNode
@@ -5,8 +6,20 @@ from test_framework import generic_test
 
 
 def even_odd_merge(L: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    if not L:
+        return None
+
+    even_dummy_head, odd_dummy_head = ListNode(0), ListNode(0)
+    turn, tails = 0, [even_dummy_head, odd_dummy_head]
+
+    while L:
+        tails[turn].next = L
+        L = L.next
+        tails[turn] = tails[turn].next
+        turn ^= 1 # switch turn to opposite
+    tails[0].next = odd_dummy_head.next
+    tails[1].next = None
+    return even_dummy_head.next
 
 
 if __name__ == '__main__':
