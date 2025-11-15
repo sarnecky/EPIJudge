d4b12ad685768f8d090044adac527abaee7ce9b2
Sebatian <ssarnecki34@gmail.com>
2025-11-15 12:07:11 +0100

    tree right sibling

--- epi_judge_python/tree_right_sibling.py
+++ epi_judge_python/tree_right_sibling.py
@@ -13,8 +13,15 @@ class BinaryTreeNode:
 
 
 def construct_right_sibling(tree: BinaryTreeNode) -> None:
-    # TODO - you fill in here.
-    return
+    def populate_children_next_field(start_node):
+        while start_node and start_node.left:
+            start_node.left.next = start_node.right
+            start_node.right.next = start_node.next and start_node.next.left
+            start_node = start_node.next
+
+    while tree and tree.left:
+        populate_children_next_field(tree)
+        tree = tree.left
 
 
 def traverse_next(node):
