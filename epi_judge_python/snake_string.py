0f8ad0425a52eb1f1cc2db4824ad7a3efb753219
Sebatian <ssarnecki34@gmail.com>
2025-11-15 10:27:26 +0100

    snake string

--- epi_judge_python/snake_string.py
+++ epi_judge_python/snake_string.py
@@ -2,8 +2,15 @@ from test_framework import generic_test
 
 
 def snake_string(s: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    result = []
+    for i in range(1, len(s), 4): # first row
+        result.append(s[i])
+    for i in range(0, len(s), 2): # second row
+        result.append(s[i])
+    for i in range(3, len(s), 4): # third row
+        result.append(s[i])
+    
+    return ''.join(result)
 
 
 if __name__ == '__main__':
