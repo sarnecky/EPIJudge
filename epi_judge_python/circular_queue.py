f4ba61a72236aa45aafa87bd5951758b6a7469b0
Sebatian <ssarnecki34@gmail.com>
2025-11-20 21:45:22 +0100

    circular queue

--- epi_judge_python/circular_queue.py
+++ epi_judge_python/circular_queue.py
@@ -3,21 +3,28 @@ from test_framework.test_failure import TestFailure
 
 
 class Queue:
+    SCALE_FACTOR = 2
     def __init__(self, capacity: int) -> None:
-        # TODO - you fill in here.
-        return
-
+        self._entries = [0] * capacity
+        self._head = self._tail = self._num_queue_elements = 0
+        
     def enqueue(self, x: int) -> None:
-        # TODO - you fill in here.
-        return
+        if self._num_queue_elements == len(self._entries):
+            self._entries = (self._entries[self._head:] + self._entries[:self._head])
+            self._head, self._tail = 0, self._num_queue_elements
+            self._entries += [0] * len(self._entries)
+        self._entries[self._tail] = x
+        self._tail = (self._tail + 1) % len(self._entries)
+        self._num_queue_elements += 1
 
     def dequeue(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        self._num_queue_elements -= 1
+        result = self._entries[self._head]
+        self._head = (self._head + 1) % len(self._entries)
+        return result
 
     def size(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        return self._num_queue_elements
 
 
 def queue_tester(ops):
